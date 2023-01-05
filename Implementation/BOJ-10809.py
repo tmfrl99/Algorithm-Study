1f7c0a9e8332c1c53f7c1a62911da5ebdf650177
tmfrl99 <93257321+tmfrl99@users.noreply.github.com>
2023-01-05 23:18:03 +0900

    [BOJ]10809

---
+++ Implementation/BOJ-10809.py
@@ -0,0 +1,8 @@
+s = str(input())
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in alpha:
+  if i in s:
+    print(s.index(i), end = " ")
+  else:
+    print(-1, end = " ")
