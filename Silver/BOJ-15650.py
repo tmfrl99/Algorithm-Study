8dcf376d03e6a24c0ae4c8bab3acd762548fcea1
tmfrl99 <93257321+tmfrl99@users.noreply.github.com>
2023-03-08 23:35:46 +0900

    N과 M(2)

---
+++ Silver/BOJ-15650.py
@@ -0,0 +1,16 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+res = []
+
+def solve(start):
+  if len(res) == m:
+    print(' '.join(map(str, res)))
+    return
+    
+  for i in range(start, n+1):
+    if i not in res:
+      res.append(i)
+      solve(i+1)
+      res.pop()
+
+solve(1)
